f99a27f959e15d6f7153834260af33921b526213
mvgv <mvgv1989@gmail.com>
2023-10-25 21:16:36 -0300

    fix: correcao de implementacao no gin com requests do apigw

--- app/main.go
+++ app/main.go
@@ -2,47 +2,50 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gin-gonic/gin"
 )
 
-func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func LambdaHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Crie uma instância do servidor Gin
 	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		resp := gin.H{
-			"statusCode": http.StatusOK,
-			"headers":    map[string]string{"Content-Type": "application/json"},
-			"body":       "Hello, World!",
-		}
-		ctx.JSON(http.StatusOK, resp)
+
+	// Defina uma rota Gin que corresponda à rota da sua API Gateway
+	r.GET("/hello", func(c *gin.Context) {
+		// Extraia os parâmetros da solicitação da API Gateway
+		name := c.DefaultQuery("name", "World")
+
+		// Execute sua lógica de negócios
+		message := "Hello, " + name
+
+		// Responda com a mensagem
+		c.JSON(200, gin.H{"message": message})
 	})
 
-	// Converte a solicitação do API Gateway em uma solicitação HTTP do Gin
-	req, err := gin.RequestFromAPIGatewayProxyRequest(&request)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}, nil
+	// Crie um contexto Gin manualmente e configure-o com a solicitação da API Gateway
+	ctx := &gin.Context{
+		Request: &http.Request{
+			Method: "GET", // Defina o método HTTP da solicitação
+			URL: &url.URL{
+				Path: "/hello", // Defina o caminho da solicitação
+			},
+			Header: make(http.Header),
+		},
+		Writer: c.Writer, // Use o gravador de resposta do Gin
 	}
 
-	// Executa a solicitação no roteador Gin
-	r.ServeHTTP(req.ResponseWriter, req)
+	// Execute a solicitação no servidor Gin
+	r.ServeHTTP(ctx.Writer, ctx.Request)
 
-	// Converte a resposta do Gin em uma resposta do API Gateway
-	resp, err := gin.APIGatewayProxyResponseFromHTTPRequest(req.Request)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       err.Error(),
-		}, nil
+	// Converta a resposta de Gin em uma resposta da API Gateway
+	response := events.APIGatewayProxyResponse{
+		StatusCode:      ctx.Writer.Status(),
+		Headers:         ctx.Writer.Header(),
+		Body:            ctx.Writer.Body.String(),
+		IsBase64Encoded: false,
 	}
 
-	return resp, nil
-}
-
-func main() {
-	lambda.Start(HandleRequest)
+	return response, nil
 }
