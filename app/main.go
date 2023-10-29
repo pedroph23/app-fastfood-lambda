eee11d4550a81c3ac0ff1a90ce85dbbd4860823c
mvgv <mvgv1989@gmail.com>
2023-10-29 01:02:59 -0300

    feat: esqueleto da lambda

--- app/main.go
+++ app/main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/example/controllers"
+	"github.com/example/repositories"
+	"github.com/example/usecases"
 )
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	respBody, err := json.Marshal("Hello World")
+func AutenticacaoClienteHandler(ctx context.Context, req events.APIGatewayProxyRequest, autenticacaoClienteUC usecases.AutenticacaoClienteUC) (events.APIGatewayProxyResponse, error) {
+	// TODO: Implementar a lógica de autenticação do cliente
+	controller := controllers.NewAutenticacaoClienteController(autenticacaoClienteUC)
+	respBody, err := controller.Handle(req.PathParameters["id_cliente"])
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, fmt.Errorf("failed to marshal response body: %v", err)
+		return events.APIGatewayProxyResponse{}, fmt.Errorf("failed to handle request: %v", err)
 	}
 
 	return events.APIGatewayProxyResponse{
@@ -27,6 +30,42 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	}, nil
 }
 
+func CadastroClienteHandler(ctx context.Context, req events.APIGatewayProxyRequest, cadastroClienteUC usecases.CadastroClienteUC) (events.APIGatewayProxyResponse, error) {
+	// TODO: Implementar a lógica de criação de cliente
+	controller := controllers.NewCadastroClienteController(cadastroClienteUC)
+	respBody, err := controller.Handle(req.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, fmt.Errorf("failed to handle request: %v", err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(respBody),
+	}, nil
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest, autenticacaoClienteUC usecases.AutenticacaoClienteUC, cadastroClienteUC usecases.CadastroClienteUC) (events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "POST":
+		if req.Path == "/clientes/{id_cliente}/auth" {
+			return AutenticacaoClienteHandler(ctx, req, autenticacaoClienteUC)
+		} else if req.Path == "/clientes" {
+			return CadastroClienteHandler(ctx, req, cadastroClienteUC)
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusNotFound,
+		Body:       http.StatusText(http.StatusNotFound),
+	}, nil
+}
+
 func main() {
-	lambda.Start(Handler)
+	clienteRepository := repositories.NewClienteRepository()
+	autenticacaoClienteUC := usecases.NewAutenticacaoClienteUC(clienteRepository)
+	cadastroClienteUC := usecases.NewCadastroClienteUC(clienteRepository)
+
+	lambda.Start(func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		return Handler(ctx, req, autenticacaoClienteUC, cadastroClienteUC)
+	})
 }
