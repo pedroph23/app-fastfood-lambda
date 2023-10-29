21c00cc1e21b37e4b895a086ea8fe203d945226a
mvgv <mvgv1989@gmail.com>
2023-10-29 20:25:16 -0300

    fix: correcao no terraform

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-1" # Substitua pela sua região
 }
 
-
 variable "TF_LAMBDA_ZIP_PATH" {
   type = string
 }
@@ -15,7 +14,6 @@ resource "aws_lambda_function" "example" {
 
   filename     = var.TF_LAMBDA_ZIP_PATH # Recupera o zip da lambda disponibilizado pela esteira
 
-
   environment {
     variables = {
       EXAMPLE_ENV_VAR = "example"
@@ -23,29 +21,42 @@ resource "aws_lambda_function" "example" {
   }
 }
 
-resource "aws_iam_role" "example" {
-  name = "example"
+resource "aws_iam_role_policy" "lambda_exec_policy" {
+  name = "crud-api-exec-role-policy"
+  role = aws_iam_role.example.id
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-      {
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
             "Action": "dynamodb:*",
             "Effect": "Allow",
             "Resource": "*"
+        }     
+      ]  
+}  
+EOF
+}
+
+resource "aws_iam_role" "example" {
+  name = "example"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "example" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.example.name
-}
+
+
+
+
