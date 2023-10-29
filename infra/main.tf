761c5b50fe63b541744801d7a8a3bae23c7e79a4
mvgv <mvgv1989@gmail.com>
2023-10-29 18:52:31 -0300

    fix: correcao no terraform

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1" # Substitua pela sua região
 }
 
+
 variable "TF_LAMBDA_ZIP_PATH" {
   type = string
 }
@@ -14,6 +15,7 @@ resource "aws_lambda_function" "example" {
 
   filename     = var.TF_LAMBDA_ZIP_PATH # Recupera o zip da lambda disponibilizado pela esteira
 
+
   environment {
     variables = {
       EXAMPLE_ENV_VAR = "example"
@@ -25,48 +27,25 @@ resource "aws_iam_role" "example" {
   name = "example"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "example" {
-  name        = "DynamoDBAccessPolicy"
-  role        = aws_iam_role.example.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+      },
       {
-        Action   = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem"
-          // Adicione outras ações conforme necessário
-        ],
-        Effect   = "Allow",
-        Resource = "arn:aws:dynamodb:us-east-1:101478099523:table/ClienteAppFastfood"
+            "Action": "dynamodb:*",
+            "Effect": "Allow",
+            "Resource": "*"
       }
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "example" {
-  policy_arn = aws_iam_role_policy.example.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.example.name
-}
-
-resource "aws_lambda_permission" "example" {
-  statement_id  = "AllowExecutionFromLambda"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.example.arn
-  principal     = "dynamodb.amazonaws.com"
-  source_arn    = "arn:aws:dynamodb:us-east-1:101478099523:table/ClienteAppFastfood"
 }
